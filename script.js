918ff7536cc2edd91d26c8ef705262acd808f9e7
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-01 17:34:05 +0000

    created functions for keyup events

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let messageAlert = document.querySelector(".jsMessageAlert");
 let submitBtn = document.querySelector(".jsSubmitBtn");
 let invalidSubmitMessage = document.querySelector(".jsInvalidSubmitMessage");
 
-// Create check element
+// Create check btn element
 function createElement(inputElement) {
   inputElement.textContent = "";
   let checkButton = document.createElement("button");
@@ -20,59 +20,92 @@ function createElement(inputElement) {
   inputElement.appendChild(checkButton).appendChild(validInput);
 }
 
-// Full name
-fullNameInput.addEventListener("keyup", () => {
+// Only return true and display check button if criteria is met, otherwise display appropriate warning message
+function validateName() {
   let name = fullNameInput.value;
   if (name.length === 0) {
     fullNameAlert.textContent = "";
+    return false
   } else if (!name.includes(" ")) {
     fullNameAlert.textContent = "Enter full name";
+    return false
   } else {
     createElement(fullNameAlert);
+    return true;
   }
-});
+}
 
-// Phone number
-phoneNumberInput.addEventListener("keyup", () => {
+// Only return true and display check button if criteria is met, otherwise display appropriate warning message
+function validateNumber() {
   let telNumber = phoneNumberInput.value;
   if (telNumber.length === 0) {
     phoneNumberAlert.textContent = "";
+    return false
   } else if (telNumber.length > 0 && !telNumber.match(/^[0-9]\d*$/)) {
     phoneNumberAlert.textContent = "Only digits please";
+    return false
   } else if (telNumber.length !== 11 && telNumber.length !== 0) {
     phoneNumberAlert.textContent = "Enter 11 digit phone No.";
+    return false
   } else {
     createElement(phoneNumberAlert);
+    return true;
   }
-});
+}
 
-// Email
-emailInput.addEventListener("keyup", () => {
+// Only return true and display check button if criteria is met, otherwise display appropriate warning message
+function validateEmail() {
   let email = emailInput.value;
   if (email.length === 0) {
     emailAlert.textContent = "";
+    return false
   } else if (!email.includes("@")) {
     emailAlert.textContent = "Enter valid email";
+    return false
   } else {
     createElement(emailAlert);
+    return true
   }
-});
+}
 
-// Message
-messageTextarea.addEventListener("keyup", () => {
+// Only return true and display check button if criteria is met, otherwise display appropriate warning message
+function validateMessage() {
   let message = messageTextarea.value;
   if (message.length === 0) {
     messageAlert.textContent = "";
+    return false
   } else if (message.length < 30) {
     messageAlert.textContent = `Enter ${
       30 - messageTextarea.value.length
     } or more characters`;
+    return false
   } else {
     createElement(messageAlert);
+    return true
   }
+}
+
+// Full name
+fullNameInput.addEventListener("keyup", () => {
+  validateName();
 });
 
-// Displays appropriate alerts upon form submission
+// Phone number
+phoneNumberInput.addEventListener("keyup", () => {
+  validateNumber();
+});
+
+// Email
+emailInput.addEventListener("keyup", () => {
+  validateEmail();
+});
+
+// Message
+messageTextarea.addEventListener("keyup", () => {
+  validateMessage();
+});
+
+// Displays appropriate alerts for each input/textarea element upon invalid form submission and sets timeout on a warning message
 let timeout;
 let timeoutActive = false;
 function displayAlert(inputAlert, message) {
@@ -98,20 +131,13 @@ function displayAlert(inputAlert, message) {
 // Submit Btn
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  // If form is valid then reset form back to initial state
   if (
-    !fullNameAlert.firstElementChild ||
-    !phoneNumberAlert.firstElementChild ||
-    !emailAlert.firstElementChild ||
-    !messageAlert.firstElementChild
+    validateName() &&
+    validateNumber() &&
+    validateEmail() &&
+    validateMessage()
   ) {
-    displayAlert(fullNameAlert, "Name required");
-    displayAlert(phoneNumberAlert, "Phone No. required");
-    displayAlert(emailAlert, "Email required");
-    displayAlert(
-      messageAlert,
-      `Enter ${30 - messageTextarea.value.length} or more characters`
-    );
-  } else {
     fullNameInput.value = "";
     phoneNumberInput.value = "";
     emailInput.value = "";
@@ -120,5 +146,14 @@ submitBtn.addEventListener("click", (e) => {
     phoneNumberAlert.firstElementChild.remove();
     emailAlert.firstElementChild.remove();
     messageAlert.firstElementChild.remove();
+    // If form is invalid then display appropriate warning message for input/textarea elements
+  } else {
+    displayAlert(fullNameAlert, "Name required");
+    displayAlert(phoneNumberAlert, "Phone No. required");
+    displayAlert(emailAlert, "Email required");
+    displayAlert(
+      messageAlert,
+      `Enter ${30 - messageTextarea.value.length} or more characters`
+    );
   }
 });
