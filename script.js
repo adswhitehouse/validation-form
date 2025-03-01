5aaf828c097f775f67796edbfdd687c525a370e2
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-01 15:27:29 +0000

    added submit button functionality

--- script.js
+++ script.js
@@ -8,21 +8,24 @@ let phoneNumberAlert = document.querySelector(".jsPhoneNumberAlert");
 let emailAlert = document.querySelector(".jsEmailAlert");
 let messageAlert = document.querySelector(".jsMessageAlert");
 let submitBtn = document.querySelector(".jsSubmitBtn");
+let invalidSubmitMessage = document.querySelector(".jsInvalidSubmitMessage");
 
 // Create check element
-let checkButton = document.createElement("button");
-let validInput = document.createElement("i");
-validInput.classList.add("fa-solid", "fa-check");
-checkButton.classList.add("check-button");
-checkButton.appendChild(validInput);
+function createElement(inputElement) {
+  inputElement.textContent = "";
+  let checkButton = document.createElement("button");
+  let validInput = document.createElement("i");
+  validInput.classList.add("fa-solid", "fa-check");
+  checkButton.classList.add("check-button");
+  inputElement.appendChild(checkButton).appendChild(validInput);
+}
 
 // Full name
 fullNameInput.addEventListener("keyup", () => {
   if (fullNameInput.value.length === 0) {
     fullNameAlert.textContent = "";
   } else if (fullNameInput.value.includes(" ")) {
-    fullNameAlert.textContent = "";
-    fullNameAlert.appendChild(checkButton);
+    createElement(fullNameAlert);
   } else {
     fullNameAlert.textContent = "Enter full name";
   }
@@ -33,8 +36,7 @@ phoneNumberInput.addEventListener("keyup", () => {
   if (phoneNumberInput.value.length === 0) {
     phoneNumberAlert.textContent = "";
   } else if (phoneNumberInput.value.length === 11) {
-    phoneNumberAlert.textContent = "";
-    phoneNumberAlert.appendChild(checkButton);
+    createElement(phoneNumberAlert);
   } else {
     phoneNumberAlert.textContent = "Enter 11 digit phone No.";
   }
@@ -45,8 +47,7 @@ emailInput.addEventListener("keyup", () => {
   if (emailInput.value.length === 0) {
     emailAlert.textContent = "";
   } else if (emailInput.value.includes("@")) {
-    emailAlert.textContent = "";
-    emailAlert.appendChild(checkButton);
+    createElement(emailAlert);
   } else {
     emailAlert.textContent = "Enter valid email";
   }
@@ -57,9 +58,45 @@ messageTextarea.addEventListener("keyup", () => {
   if (messageTextarea.value.length === 0) {
     messageAlert.textContent = "";
   } else if (messageTextarea.value.length >= 30) {
-    messageAlert.textContent = "";
-    messageAlert.appendChild(checkButton);
+    createElement(messageAlert);
   } else {
-    messageAlert.textContent = "Enter 30 or more characters";
+    messageAlert.textContent = `Enter ${
+      30 - messageTextarea.value.length
+    } or more characters`;
   }
+});
+
+// Displays appropriate alerts upon form submission
+let timeout;
+let timeoutActive = false;
+function displayAlert(inputAlert, message) {
+  if (!inputAlert.firstElementChild) {
+    inputAlert.textContent = `${message}`;
+    invalidSubmitMessage.style.visibility = "visible";
+    if (!timeoutActive) {
+      timeoutActive = true;
+      timeout = setTimeout(() => {
+        invalidSubmitMessage.style.visibility = "hidden";
+        timeoutActive = false;
+      }, 1000);
+    } else {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        invalidSubmitMessage.style.visibility = "hidden";
+        timeoutActive = false;
+      }, 1000);
+    }
+  }
+}
+
+// Submit Btn
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  displayAlert(fullNameAlert, "Name required");
+  displayAlert(phoneNumberAlert, "Phone No. required");
+  displayAlert(emailAlert, "Email required");
+  displayAlert(
+    messageAlert,
+    `Enter ${30 - messageTextarea.value.length} or more characters`
+  );
 });
